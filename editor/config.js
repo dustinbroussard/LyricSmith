2328428359d4a5b18015278b944443ed2df8b378
dustinbroussard <dustin.broussard@gmail.com>
2025-08-03 01:53:55 -0500

    feat: add editor config file with initial settings

---
+++ editor/config.js
@@ -0,0 +1,12 @@
+// config.js
+
+export const CONFIG = {
+  chordsModeEnabled: true,
+  autosaveEnabled: true,
+  autoscrollDefaultEnabled: true,
+  showExperimentalFeatures: false,
+  maxFontSize: 64,
+  minFontSize: 16,
+  devMode: true, // use this to show extra info/buttons
+}
+
