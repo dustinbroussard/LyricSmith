24637c759ee524c3f0a6fc428c9cac41e1c04008
dustinbroussard <dustin.broussard@gmail.com>
2025-08-03 19:34:50 -0500

    added a bunch of stuff.  broke the Ui.  Fixed it?  almost

---
+++ editor/config.js
@@ -0,0 +1,19 @@
+window.CONFIG = {
+  chordsModeEnabled: true,
+  autosaveEnabled: true,
+  autoscrollDefaultEnabled: true,
+  showExperimentalFeatures: false,
+  maxFontSize: 64,
+  minFontSize: 16,
+  devMode: true,
+};
+
+window.CONFIG = {
+  chordsModeEnabled: true,
+  autosaveEnabled: true,
+  autoscrollDefaultEnabled: true,
+  showExperimentalFeatures: false,
+  maxFontSize: 64,  
+  minFontSize: 16,
+  devMode: true
+};
